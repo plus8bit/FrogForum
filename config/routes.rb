abca567c76e65e5a0f137d23ea700fa9d55cbbc4
Nick <plus0bit@gmail.com>
2018-12-11 22:39:39 +0300

    Add styling to buttons and tags

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   resources :discussions
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'discussions#index'
 end
