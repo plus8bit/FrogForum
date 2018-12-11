525c22829a38ad28c75328c2e4d54be7cfe8b6b0
Nick <plus0bit@gmail.com>
2018-12-12 02:21:15 +0300

    Add pry-rails gem

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,7 @@
+class SessionsController < ApplicationController
+
+  def new
+    binding.pry
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :discussions
   root 'discussions#index'
+
+  get '/auth/:provider/callback' => 'sessions#new'
 end
